c36ae4b68fa4edf71ac1b3839d6188d1b46c7e0b
dariaKretiuk <kretyukdasha@gmail.com>
2022-03-24 22:46:50 +0700

    Add project

---
+++ iOS5-HW13-TableSettings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS5-HW13-TableSettings
+//
+//  Created by Дарья Кретюк on 24.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
